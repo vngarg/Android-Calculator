7b86e770cdcade75ad1ebb5393680fc509701586
vngarg <sngargrsd@gmail.com>
2020-03-27 00:45:38 +0530

    Calculator Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,6 +25,61 @@ class SimpleCalculator extends StatefulWidget {
 }
 
 class _SimpleCalculatorState extends State<SimpleCalculator> {
+
+  String equation = '0';
+  String result = '0';
+  String expression = '';
+  double equationFontSize = 38.0;
+  double resultFontSize = 48.0;
+
+  buttonPressed(String buttonText) {
+    setState(() {
+      if(buttonText == 'C') {
+        equation = '0' ;
+        result = '0' ;
+        equationFontSize = 38.0 ;
+        resultFontSize = 48.0 ;
+      } 
+      else if(buttonText == '⌫') {
+        equationFontSize = 48.0 ; 
+        resultFontSize = 38.0 ;
+        equation = equation.substring(0 , equation.length - 1) ;
+        
+        if(equation == '')
+        equation = '0' ;
+      } 
+      else if(buttonText == '=') {
+        equationFontSize = 38.0 ;
+        resultFontSize = 48.0 ;
+
+        expression = equation;
+        expression = expression.replaceAll('×', '*');
+        expression = expression.replaceAll('+', '/');
+
+        try {
+          Parser p =Parser() ;
+          Expression exp = p.parse(expression);
+
+          ContextModel cm = ContextModel();
+          result = '${exp.evaluate(EvaluationType.REAL, cm)}';
+        }
+        catch(error) {
+          result = 'Error';
+        }
+      } 
+      else {
+        equationFontSize = 48.0 ; 
+        resultFontSize = 38.0 ;
+
+        if(equation =='0') {
+          equation = buttonText ;
+        } else {
+          equation = equation + buttonText;
+        }
+      }
+    });
+  }
+
   Widget buildButton(
       String buttonText, double buttonHeight, Color buttonColor) {
     return Container(
@@ -33,6 +91,7 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
             side: BorderSide(
                 color: Colors.white, width: 1, style: BorderStyle.solid)),
         padding: EdgeInsets.all(16.0),
+        onPressed: () => buttonPressed(buttonText),
         child: Text(
           buttonText,
           style: TextStyle(
@@ -57,17 +116,17 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
           Container(
             alignment: Alignment.centerRight,
             padding: EdgeInsets.fromLTRB(10, 20, 20, 0),
-            child: Text('0',
+            child: Text(equation,
                 style: TextStyle(
-                  fontSize: 38.0,
+                  fontSize: equationFontSize,
                 )),
           ),
           Container(
             alignment: Alignment.centerRight,
             padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
-            child: Text('0',
+            child: Text(result,
                 style: TextStyle(
-                  fontSize: 48.0,
+                  fontSize: resultFontSize,
                 )),
           ),
           Expanded(
